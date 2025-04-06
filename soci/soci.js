9cf08cd574b4dc1e87218757a5f7275fccbc7ecd
loressm <lorerome90@virgilio.it>
2025-04-06 15:04:30 +0200

    Add files via upload

---
+++ soci/soci.js
@@ -0,0 +1,8 @@
+function toggleMemberDetails(memberId) {
+    const details = document.getElementById(memberId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
