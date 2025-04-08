2ae97c73ab2d380346a35591fc852b26a9f466ca
loressm <lorerome90@virgilio.it>
2025-04-08 11:00:22 +0200

    Delete spazi/spazi.js

--- spazi/spazi.js
+++
@@ -1,8 +0,0 @@
-function toggleSpaceDetails(spaceId) {
-    const details = document.getElementById(spaceId);
-    if (details.style.display === "none" || details.style.display === "") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
